365decca40dfa7febc930487f7abea0eb8b2bae6
Jhan Mariano <jhanmariano.github@gmail.com>
2023-09-22 18:00:24 +0800

    'addtag'

--- src/main/java/com/example/test/TestController.java
+++ src/main/java/com/example/test/TestController.java
@@ -3,17 +3,19 @@ package com.example.test;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Test Controller", description = "Sample test controller")
 public class TestController {
 
     @GetMapping("/test")
     @Operation(summary = "Simple get method")
-    @ApiResponse(responseCode = "200", description = "get operation sucesss")
+    @ApiResponse(responseCode = "200", description = "get operation success")
     public ResponseEntity<Void> test() {
         return new ResponseEntity<>(HttpStatus.OK);
     }
